fec2f2d8a99f973a58467e6a5b6be784a900a5bc
Israel Berger <fake@mail.com>
2019-05-06 20:47:18 +0300

    add mnist

---
+++ mnist_test.py
@@ -0,0 +1,6 @@
+from mnist import MNIST
+mndata = MNIST(path='./MNIST', mode='rounded_binarized', return_type='numpy', gz=False)
+images, labels = mndata.load_training()
+images = images[:10]
+labels = labels[:10]
+pass
