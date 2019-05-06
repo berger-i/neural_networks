0cc8d1fa133531fb884b5c7d49c9aaad36fc9902
Israel Berger <fake@mail.com>
2019-05-06 23:53:51 +0300

    code cosmetics

--- SLP.py
+++ SLP.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 class SLP(object):
-    def __init__(self, num_of_input_neurons, num_of_output_neurons, activation=None):        
+    def __init__(self, num_of_input_neurons, num_of_output_neurons, activation=None):
         self.num_of_input_neurons = num_of_input_neurons
         self.num_of_output_neurons = num_of_output_neurons
         self.activation = activation
 
-        self.weights = np.random.rand(num_of_output_neurons, num_of_input_neurons+1) #+1 for bias        
+        self.weights = np.random.rand(num_of_output_neurons, num_of_input_neurons+1) #+1 for bias
 
     def __str__(self):
         out_str = 'num_of_input_neurons=' + str(self.num_of_input_neurons)
         out_str += '\nnum_of_output_neurons=' + str(self.num_of_output_neurons)
         out_str += '\nweights=\n' + str(self.weights)
-        return out_str        
+        return out_str
 
     def learn(self, learning_sample):
         # delta_w_i = ni * o_i * delta_omega
@@ -24,9 +24,8 @@ class SLP(object):
             delta_w_data = np.transpose(ni * x * delta_omega)
             if delta_w_data.shape == (1,):
                 delta_w_data = [delta_w_data]
-            delta_w_total = np.hstack((delta_w_bias,delta_w_data))
+            delta_w_total = np.hstack((delta_w_bias, delta_w_data))
             self.weights -= delta_w_total
-        pass
 
     def calculate_error_function(self, testing_sample):
         sum_squares = 0
@@ -40,19 +39,16 @@ class SLP(object):
         product = np.matmul(self.weights, x_with_bias)
         y = product.sum(axis=1)
         if self.activation == 'heaviside':
-            #TODO fix pylint
-            # pylint: disable=E1103
             y = np.heaviside(y, 0)
-            # pylint: enable=E1103
 
 
         #print('self.weights\n',self.weights)
         #print('x\n',x)
         #print ('product\n',product)
         #print ('y\n',y)
-        return y   
+        return y
 
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     pass
--- test_SLP.py
+++ test_SLP.py
@@ -1,31 +1,32 @@
 import unittest
-import SLP
 import numpy as np
+import SLP
 
-class Test_SLP(unittest.TestCase):        
+class Test_SLP(unittest.TestCase):
     def generate_learning_sample_sine(self, sample_size):
         learning_sample = []
         for _ in range(sample_size):
             x = np.random.random(1) * np.pi * 2 - np.pi
             y = np.sin(x)
-            learning_sample.append((x,y))
+            learning_sample.append((x, y))
         return learning_sample
 
     def generate_learning_sample_identity(self, sample_size):
         learning_sample = []
         for _ in range(sample_size):
-            x = np.random.random(1)         
-            learning_sample.append((x,x))
+            x = np.random.random(1)
+            learning_sample.append((x, x))
         return learning_sample
-    
+
+
     def generate_learning_sample_and(self, sample_size):
         learning_sample = []
         for _ in range(sample_size):
-            x1 = np.random.random_integers(0,1)
-            x2 = np.random.random_integers(0,1)
-            x = np.array([[x1],[x2]])
-            y = x1 & x2        
-            learning_sample.append((x,y))
+            x1 = np.random.random_integers(0, 1)
+            x2 = np.random.random_integers(0, 1)
+            x = np.array([[x1], [x2]])
+            y = x1 & x2
+            learning_sample.append((x, y))
         return learning_sample
 
     def test_sine(self):
@@ -36,15 +37,15 @@ class Test_SLP(unittest.TestCase):
 
         learning_sample = self.generate_learning_sample_sine(1000)
         testing_sample = self.generate_learning_sample_sine(1000)
-        
+
         before = my_net.calculate_error_function(testing_sample)
         my_net.learn(learning_sample)
         after = my_net.calculate_error_function(testing_sample)
-        
-        print (before, after)
-        print (my_net.weights)
 
-        self.assertGreater(before,after)
+        print(before, after)
+        print(my_net.weights)
+
+        self.assertGreater(before, after)
 
     def test_identity(self):
         num_of_input_neurons = 1
@@ -53,15 +54,15 @@ class Test_SLP(unittest.TestCase):
 
         learning_sample = self.generate_learning_sample_identity(100000)
         testing_sample = self.generate_learning_sample_identity(100000)
-        
+
         before = my_net.calculate_error_function(testing_sample)
         my_net.learn(learning_sample)
         after = my_net.calculate_error_function(testing_sample)
-        
-        print (before, after)
-        print (my_net.weights)
 
-        self.assertGreater(before,after)
+        print(before, after)
+        print(my_net.weights)
+
+        self.assertGreater(before, after)
 
     def test_and(self):
         num_of_input_neurons = 2
@@ -70,14 +71,13 @@ class Test_SLP(unittest.TestCase):
 
         learning_sample = self.generate_learning_sample_and(1000)
         testing_sample = self.generate_learning_sample_and(1000)
-        
+
         before = my_net.calculate_error_function(testing_sample)
         my_net.learn(learning_sample)
         after = my_net.calculate_error_function(testing_sample)
 
 
-        self.assertGreater(before,after)
-    pass
+        self.assertGreater(before, after)
 
 if __name__ == '__main__':
     unittest.main()
